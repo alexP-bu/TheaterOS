fd251be38448d776513735621af289a491dcfd80
alexP-bu <apozin@bu.edu>
2021-12-14 23:41:29 -0500

    updated ticket class

--- code/src/theaterProjectCode/Ticket.java
+++ code/src/theaterProjectCode/Ticket.java
@@ -3,73 +3,23 @@ package theaterProjectCode;
 import java.io.Serializable;
 
 public class Ticket implements Serializable {
-	private Showtime show;
-	private String theater;
-	private Seat seat;
+	private String showtimeID;
+	private String theaterID;
+	private String seatID;
 	private double price;
-	private Date datePurchased = null;
+	private Date datePurchased;
 
-	public Ticket(Showtime show, Seat seat, double price, Date datePurchased, String theaterID) {
-		this.show = show;
-		this.seat = seat;
+	public Ticket(String sID, String tID, String seatID, double price, Date date) {
+		this.showtimeID = sID;
+		this.theaterID = tID;
+		this.seatID = seatID;
 		this.price = price;
-		this.datePurchased = datePurchased;
-		this.theater = theaterID;
-	}
-
-	public Ticket() {
-		this.show = null;
-		this.seat = null;
-		price = 0.00;
-		this.datePurchased = null;
-	}
-
-	/*
-	 * getters/setters
-	 */
-
-	public Showtime getShow() {
-		return show;
-	}
-
-	public void setShow(Showtime show) {
-		this.show = show;
-	}
-
-	public Seat getSeat() {
-		return seat;
-	}
-
-	public void setSeat(Seat seat) {
-		this.seat = seat;
-	}
-
-	public double getPrice() {
-		return price;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-	public Date getDatePurchased() {
-		return datePurchased;
-	}
-
-	public void setDatePurchased(Date datePurchased) {
-		this.datePurchased = datePurchased;
-	}
-
-	public String getTheater() {
-		return theater;
-	}
-
-	public void setTheater(String theater) {
-		this.theater = theater;
+		this.datePurchased = date;
 	}
 
 	@Override
 	public String toString() {
-		return "\n" + this.show.getName() + " Showing on " + this.show.getDate() + "\nPrice: " + this.show.getPrice();
+		return "Ticket Purchased For: " + showtimeID + ", theater " + theaterID + ", seat " + seatID + ", " + price + "\n"
+				+ "Purchase date: " + datePurchased;
 	}
 }
